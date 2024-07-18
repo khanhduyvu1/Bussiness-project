abe373fe269cc54bc4fc7fd483b9704e6fd189b9
Khanh Vu <khanhduyvu@usf.edu>
2024-07-18 15:30:31 +0700

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './component/userlogin/Login'; // Make sure the path is correct based on your file structure
-import SignUp from './component/userlogin/signup';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<SignUp />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
